6092c318e674f136569d2e323aefc05f36b71728
ahamdy <ahmed.hamdy@zalando.de>
2017-08-24 11:07:35 +0200

    adding more tests in ScaleManagerTest

--- src/test/scala/io/ahamdy/taskforce/common/DummyTime.scala
+++ src/test/scala/io/ahamdy/taskforce/common/DummyTime.scala
@@ -3,8 +3,15 @@ package io.ahamdy.taskforce.common
 import java.time.ZonedDateTime
 import java.util.concurrent.atomic.AtomicReference
 
+import io.ahamdy.taskforce.syntax.zonedDateTime._
+
+import scala.concurrent.duration._
+
 
 class DummyTime(now: ZonedDateTime) extends Time {
   val currentTime = new AtomicReference(now)
   override def unsafeNow(): ZonedDateTime = currentTime.get()
+
+  def increaseBy(duration: FiniteDuration): Unit = currentTime.set(currentTime.get().plus(duration))
+  def decreaseBy(duration: FiniteDuration): Unit = currentTime.set(currentTime.get().minus(duration))
 }
